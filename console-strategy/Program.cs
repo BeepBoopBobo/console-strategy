e9a071a4012079d94465f38150d0f9331fd20038
BeepBoopBobo <marek.tomas.mk@gmail.com>
2024-04-25 18:37:14 +0200

    add: base resource class

--- console-strategy/Program.cs
+++ console-strategy/Program.cs
@@ -1,6 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using console_strategy;
 
-ConsoleHandler console = new ConsoleHandler();
 
-console.ReadInput();
+Resource wood = new Resource("Wood", 100, 250);
+Resource stone = new Resource("Stone", 150, 250);
+Resource gold = new Resource("Gold", 150, 250);
+
+
+
+Resource[] resources = { wood, stone, gold };
---
+++ console-strategy/Resource.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace console_strategy
+{
+    internal class Resource
+    {
+        private string name;
+        private int amount;
+        private int capacity;
+
+        public Resource(string name, int amount=0, int capacity=0) {
+            this.name = name;
+            this.amount = amount;
+            this.capacity = capacity;
+        }
+        public string Name { 
+            get { return name; } 
+        }
+        public int Amount { 
+            set { this.amount = value; }
+            get { return amount; } 
+        }
+        public int Capacity { 
+            set { this.capacity = value; }
+            get { return capacity; } 
+        }
+
+        public void ChangeAmount(int value)
+        {
+            this.Amount += value;
+        }
+
+        public void ChangeCapacity(int value)
+        {
+            this.Capacity += value;
+        }
+
+    }
+}
